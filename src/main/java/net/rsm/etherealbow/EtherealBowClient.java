0a6baf40a78eea0fef54e0b3ffd8a721c6e93e87
Atlearia <ronanthenickname@gmail.com>
2025-07-04 23:26:21 +0200

    Initial commit

---
+++ src/main/java/net/rsm/etherealbow/EtherealBowClient.java
@@ -0,0 +1,10 @@
+package net.rsm.etherealbow;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EtherealBowClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
