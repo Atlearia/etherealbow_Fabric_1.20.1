64a209a33526d385d36fb2184d97f1a0153401b8
Atlearia <ronanthenickname@gmail.com>
2025-07-07 01:33:39 +0200

    bruh

--- src/main/java/net/rsm/etherealbow/FirstMod.java
+++ src/main/java/net/rsm/etherealbow/FirstMod.java
@@ -2,17 +2,18 @@ package net.rsm.etherealbow;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.rsm.etherealbow.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class EtherealBow implements ModInitializer {
-	public static final String MOD_ID = "etherealbow";
+public class FirstMod implements ModInitializer {
+	public static final String MOD_ID = "firstmod";
 
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
-
+		ModItems.registerModItems();
 
 		LOGGER.info("Hello Fabric world!");
 	}
--- src/main/java/net/rsm/etherealbow/FirstModDataGenerator.java
+++ src/main/java/net/rsm/etherealbow/FirstModDataGenerator.java
@@ -3,7 +3,7 @@ package net.rsm.etherealbow;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
-public class EtherealBowDataGenerator implements DataGeneratorEntrypoint {
+public class FirstModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
 
