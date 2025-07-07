12442b590df659ad55ca85e8f3b94994edc351f5
Atlearia <ronanthenickname@gmail.com>
2025-07-08 00:40:46 +0200

    fml

--- src/main/java/net/rsm/etherealbow/item/ModItems.java
+++ src/main/java/net/rsm/etherealbow/item/ModItems.java
@@ -1,6 +1,7 @@
 package net.rsm.etherealbow.item;
 
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.BowItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemGroups;
@@ -10,7 +11,7 @@ import net.minecraft.util.Identifier;
 import net.rsm.etherealbow.FirstMod;
 
 public class ModItems {
-    public static final Item ETHEREAL_BOW = registerItem("etherealbow", new Item(new Item.Settings()));
+    public static final Item ETHEREAL_BOW = registerItem("etherealbow", new BowItem(new Item.Settings().maxDamage(384)));
 
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(FirstMod.MOD_ID, name), item);
@@ -20,7 +21,7 @@ public class ModItems {
     {
         FirstMod.LOGGER.info("Registering Mod Items for" + FirstMod.MOD_ID);
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
-        entries.add(ETHEREAL_BOW);
+            entries.add(ETHEREAL_BOW);
 
         });
     }
