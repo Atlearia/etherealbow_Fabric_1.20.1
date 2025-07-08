e6c2e90e60f2e85a6a15aa75a0959ca2e45875d5
Atlearia <ronanthenickname@gmail.com>
2025-07-08 04:43:39 +0200

    life is great

--- src/main/java/net/rsm/etherealbow/FirstModClient.java
+++ src/main/java/net/rsm/etherealbow/FirstModClient.java
@@ -1,10 +1,16 @@
 package net.rsm.etherealbow;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.rsm.etherealbow.util.ModModelPredicates;
 
+@Environment(EnvType.CLIENT)
 public class FirstModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-
+        // this is where we wire up your pull/pulling predicates
+        ModModelPredicates.registerModelPredicates();
+        System.out.println("[EtherealBow] Model predicates registered on client");
     }
 }
